e9baa0187e61b8fcb10310258573c94cd6420d2e
Montu Meena <158208396+montumeena141@users.noreply.github.com>
2025-02-13 12:52:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //your JS code here. If required.
 
-// let cap= document.getElementById("fname");
-//  cap.addEventListener("blur", () {
-//     // this.value = this.value.toUpperCase();
-// 	 let val=cap.value;
-// 	cap.value = val.toUpperCase();
-//  });
+let cap= document.getElementById("fname");
+ cap.addEventListener("blur", ()=> {
+    // this.value = this.value.toUpperCase();
+	 let val=cap.value;
+	cap.value = val.toUpperCase();
+ });
 
-let input = document.getElementById("fname");
-	input.addEventListener("blur",()=>{
-		let val = input.value;
-		input.value = val.toUpperCase();
-	})
+// let input = document.getElementById("fname");
+// 	input.addEventListener("blur",()=>{
+// 		let val = input.value;
+// 		input.value = val.toUpperCase();
+// 	})
