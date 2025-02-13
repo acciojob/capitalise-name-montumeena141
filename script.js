a6eac7230fca0588543c2318dac8e4ebf366df58
Montu Meena <158208396+montumeena141@users.noreply.github.com>
2025-02-13 12:43:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+
+let cap= document.getElementById("fname");
+ cap.addEventListener("blur", function() {
+            this.value = this.value.toUpperCase();
+        });
